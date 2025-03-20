414f34e8f8f52cab526fe39d2b99703e806a296f
ElisaSantini <124352917+ElisaSantini@users.noreply.github.com>
2025-03-20 22:50:28 +0100

    joker_implementation

--- src/synthesis/source/JokerReachabilitySynthesizer.cpp
+++ src/synthesis/source/JokerReachabilitySynthesizer.cpp
@@ -1,34 +1,37 @@
-#include "ReachabilitySynthesizer.h"
-
+#include "JokerReachabilitySynthesizer.h"
 #include <cassert>
 
 namespace Syft {
 
-ReachabilitySynthesizer::ReachabilitySynthesizer(SymbolicStateDfa spec,
-						 Player starting_player, Player protagonist_player,
+  JokerReachabilitySynthesizer::JokerReachabilitySynthesizer(SymbolicStateDfa spec,
+						Player starting_player, Player protagonist_player,
 						 CUDD::BDD goal_states,
+             CUDD::BDD goal_moves, //ELISA CHANGED
 						 CUDD::BDD state_space)
-    : DfaGameSynthesizer(spec, starting_player, protagonist_player)
-    , goal_states_(goal_states), state_space_(state_space)
+    : JokerDfaGameSynthesizer(spec, starting_player, protagonist_player)
+    , goal_states_(goal_states), goal_moves_(goal_moves), state_space_(state_space) //ELISA CHANGED
 {}
 
 
-SynthesisResult ReachabilitySynthesizer::run() const {
+SynthesisResult JokerReachabilitySynthesizer::run() const { ///ELISA CHANGED
   SynthesisResult result;
   CUDD::BDD winning_states = state_space_ & goal_states_;
-  CUDD::BDD winning_moves = winning_states;
+  CUDD::BDD winning_moves = goal_moves_; //ELISA CHANGED
 
   while (true) {
-    CUDD::BDD new_winning_moves = winning_moves |
-                                  (state_space_ & (!winning_states) & preimage(winning_states));
+    CUDD::BDD new_winning_moves_star = winning_moves |                                           
+                                  (state_space_ & (!winning_states) & preimage_star(winning_states));    //t_star
 
-    CUDD::BDD new_winning_states = project_into_states(new_winning_moves);
+    CUDD::BDD new_winning_states_star = project_into_states(new_winning_moves_star);        //w_star
 
+    CUDD::BDD new_winning_moves = new_winning_moves_star |                                           
+                                  (state_space_ & (!new_winning_states_star) & preimage(new_winning_states_star));    //t_hat
+
+    CUDD::BDD new_winning_states = project_into_states(new_winning_moves);        //w_hat
 
     if (includes_initial_state(new_winning_states)) {
         result.realizability = true;
         result.winning_states = new_winning_states;
-        result.winning_moves = new_winning_moves; //ELISA CHANGED
         std::unordered_map<int, CUDD::BDD> strategy = synthesize_strategy(
               new_winning_moves);
 
@@ -40,15 +43,15 @@ SynthesisResult ReachabilitySynthesizer::run() const {
     } else if (new_winning_states == winning_states) {
         result.realizability = false;
         result.winning_states = new_winning_states;
-        result.winning_moves = new_winning_moves; //ELISA CHANGED
         // result.transducer = nullptr;
         std::unordered_map<int, CUDD::BDD> strategy = synthesize_strategy(
-              new_winning_moves);
+          new_winning_moves);
 
         result.transducer = std::make_unique<Transducer>(
               var_mgr_, initial_vector_, strategy, spec_.transition_function(),
               starting_player_, protagonist_player_);
         return result;
+    
     }
 
     winning_moves = new_winning_moves;
